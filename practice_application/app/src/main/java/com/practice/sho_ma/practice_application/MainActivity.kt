0bf0ca5797d53fbb2dbf155f3c06729d132b71f6
stealpattan <stealpattan@gmail.com>
2017-06-21 11:20:56 +0900

    made button

--- practice_application/app/src/main/java/com/practice/sho_ma/practice_application/MainActivity.kt
+++ practice_application/app/src/main/java/com/practice/sho_ma/practice_application/MainActivity.kt
@@ -5,11 +5,21 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 
+// add librarys here
+import android.widget.*
 
 class MainActivity : ActionBarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView1 = findViewById(R.id.textView) as TextView
+        val button1 = findViewById(R.id.button1) as Button
+
+        button1.setOnClickListener{
+            v ->
+            textView1.text = "hogehoge"
+        }
     }
 }
