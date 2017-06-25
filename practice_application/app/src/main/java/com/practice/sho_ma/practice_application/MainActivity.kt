4b4539fb5da52076a4cbbf4ef5098ff2cb7dbd49
stealpattan <stealpattan@gmail.com>
2017-06-25 17:03:11 +0900

    http communication done... using okhttp library

--- practice_application/app/src/main/java/com/practice/sho_ma/practice_application/MainActivity.kt
+++ practice_application/app/src/main/java/com/practice/sho_ma/practice_application/MainActivity.kt
@@ -9,18 +9,33 @@ import android.view.MenuItem
 import android.widget.*
 import org.json.JSONObject
 import java.util.ArrayList
+import okhttp3.*
+import android.os.AsyncTask
+import android.util.Log
+
+//okhttp class
+open class MyAsyncTask: AsyncTask<Void,Void,String>(){
+    override fun doInBackground(vararg params: Void):String?{
+        return null
+    }
+    override fun onPostExecute(text: String){}
+}
 
 class MainActivity : ActionBarActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //widget initialize here
         val textView1 = findViewById(R.id.textView) as TextView
         val button1 = findViewById(R.id.button1) as Button
         val button2 = findViewById(R.id.button2) as Button
+        val button3 = findViewById(R.id.button3) as Button
 
+        //json test text
         val testString = "{\"screen_name\":\"katty0324\",\"age\":23}";
+
+        //listview damy text
         val testArray = ArrayList<Int>() as ArrayList<Int>
         var i = 0
         while(i < 100){
@@ -29,6 +44,10 @@ class MainActivity : ActionBarActivity() {
         }
         val listAdapter = ArrayAdapter(this, R.layout.list_content, testArray)
 
+        //okhttp3 initialize here
+        val client = OkHttpClient() as OkHttpClient
+
+        //widget code here
         button1.setOnClickListener{
             v ->
             textView1.text = "hogehoge"
@@ -47,10 +66,34 @@ class MainActivity : ActionBarActivity() {
             val listview1 = findViewById(R.id.listView1) as ListView
             listview1.setAdapter(listAdapter)
         }
-    }
 
+        button3.setOnClickListener{
+            v ->
+        }
+        object: MyAsyncTask(){
+            override fun doInBackground(vararg params: Void): String?{
+                return run()
+            }
+            override fun onPostExecute(text: String){
+                textView1.text = text
+            }
+        }.execute()
+    }
     fun makeToast(massage: String){
         val toast = Toast.makeText(this, massage, Toast.LENGTH_SHORT)
         toast.show()
+    }
+
+    //get html function
+    fun run(): String?{
+        try {
+            val client = OkHttpClient() as OkHttpClient
+            val req = Request.Builder().url("http://square.github.io/okhttp/").get().build()
+            val resp = client.newCall(req).execute()
+            return resp.body()?.string()
+        }catch(e: Exception){
+            Log.e("error", "error", e)
+            return "error"
+        }
     }
 }
