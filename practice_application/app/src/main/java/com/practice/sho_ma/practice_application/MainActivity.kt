43a530d4f96cb184093015b3b51096f58d410888
stealpattan <stealpattan@gmail.com>
2017-07-03 15:05:48 +0900

    achive make swipe action. some file I deleted

--- practice_application/app/src/main/java/com/practice/sho_ma/practice_application/MainActivity.kt
+++ practice_application/app/src/main/java/com/practice/sho_ma/practice_application/MainActivity.kt
@@ -5,95 +5,23 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 
-// add librarys here
+//add library
 import android.widget.*
-import org.json.JSONObject
-import java.util.ArrayList
-import okhttp3.*
-import android.os.AsyncTask
-import android.util.Log
+//these are used by swipe action
+import android.support.v4.app.FragmentActivity
+import android.support.v4.view.ViewPager
+import com.practice.sho_ma.practice_application.MyPagerAdapter
 
-//okhttp class
-open class MyAsyncTask: AsyncTask<Void,Void,String>(){
-    override fun doInBackground(vararg params: Void):String?{
-        return null
-    }
-    override fun onPostExecute(text: String){}
-}
-
-class MainActivity : ActionBarActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //widget initialize here
-        val textView1 = findViewById(R.id.textView) as TextView
-        val button1 = findViewById(R.id.button1) as Button
-        val button2 = findViewById(R.id.button2) as Button
-        val button3 = findViewById(R.id.button3) as Button
-
-        //json test text
-        val testString = "{\"screen_name\":\"katty0324\",\"age\":23}";
-
-        //listview damy text
-        val testArray = ArrayList<Int>() as ArrayList<Int>
-        var i = 0
-        while(i < 100){
-            testArray.add(i)
-            i = i + 1
-        }
-        val listAdapter = ArrayAdapter(this, R.layout.list_content, testArray)
-
-        //okhttp3 initialize here
-        val client = OkHttpClient() as OkHttpClient
-
-        //widget code here
-        button1.setOnClickListener{
-            v ->
-            textView1.text = "hogehoge"
-            makeToast("hogehoge")
-            try{
-                var jsonobject = JSONObject(testString) as JSONObject
-                makeToast("json test :" + jsonobject.getString("screen_name"))
-            }catch(e: Exception){
-                makeToast("miss")
-            }
-        }
-
-        button2.setOnClickListener{
-            v ->
-            setContentView(R.layout.list_activity)
-            val listview1 = findViewById(R.id.listView1) as ListView
-            listview1.setAdapter(listAdapter)
-        }
-
-        button3.setOnClickListener{
-            v ->
-        }
-        object: MyAsyncTask(){
-            override fun doInBackground(vararg params: Void): String?{
-                return run()
-            }
-            override fun onPostExecute(text: String){
-                textView1.text = text
-            }
-        }.execute()
+        val viewPager = findViewById(R.id.pager) as ViewPager
+        viewPager.setAdapter(MyPagerAdapter(getSupportFragmentManager()))
     }
     fun makeToast(massage: String){
         val toast = Toast.makeText(this, massage, Toast.LENGTH_SHORT)
         toast.show()
-    }
-
-    //get html function
-    fun run(): String?{
-        try {
-            val client = OkHttpClient() as OkHttpClient
-            val req = Request.Builder().url("http://square.github.io/okhttp/").get().build()
-            val resp = client.newCall(req).execute()
-            return resp.body()?.string()
-        }catch(e: Exception){
-            Log.e("error", "error", e)
-            return "error"
-        }
     }
 }
