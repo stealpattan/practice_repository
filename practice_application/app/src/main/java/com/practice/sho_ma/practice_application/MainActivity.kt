a0794a4ffc22aa0336e7d84441b7dc1c9ef47d82
stealpattan <stealpattan@gmail.com>
2017-10-16 22:55:09 +0900

    network and button

--- practice_application/app/src/main/java/com/practice/sho_ma/practice_application/MainActivity.kt
+++ practice_application/app/src/main/java/com/practice/sho_ma/practice_application/MainActivity.kt
@@ -2,28 +2,63 @@ package com.practice.sho_ma.practice_application
 
 import android.support.v7.app.ActionBarActivity
 import android.os.Bundle
+import android.os.AsyncTask
 import android.view.Menu
 import android.view.MenuItem
-
 //add library
 import android.widget.*
-//these are used by swipe action
-import android.support.v4.app.FragmentActivity
-import android.support.v4.view.ViewPager
-import com.practice.sho_ma.practice_application.MyPagerAdapter
+//http connection
+import okhttp3.*
+//JSON
+import org.json.JSONObject
 
-class MainActivity : FragmentActivity() {
+open class MyAsyncTask:AsyncTask<Void,Void,String>(){
+    override fun doInBackground(vararg param: Void):String?{
+        return null
+    }
+    override fun onPostExecute(text:String){}
+}
+
+class MainActivity : ActionBarActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //widgets initialize here
-        val viewPager = findViewById(R.id.pager) as ViewPager
+        val testbutton = findViewById(R.id.testbutton) as Button
+        val testtext = findViewById(R.id.testtext) as TextView
 
-        viewPager.setAdapter(MyPagerAdapter(getSupportFragmentManager()))
+        testbutton.setOnClickListener{
+            v->
+            object:MyAsyncTask(){
+                override fun doInBackground(vararg param: Void):String?{
+                    return run()
+                }
+                override fun onPostExecute(text: String){
+                    //testtext.setText(text)
+                    try{
+                        var json_content = JSONObject(text) as JSONObject
+                        val str = json_content.getStirng(0).getString("id")
+                        testtext.setText(str)
+                    }catch(e: Exception){
+                        testtext.setText(e)
+                    }
+                }
+            }.execute()
+        }
     }
     fun makeToast(massage: String){
         val toast = Toast.makeText(this, massage, Toast.LENGTH_SHORT)
         toast.show()
+    }
+    //http connection function
+    fun run():String?{
+        try{
+            val client = OkHttpClient() as OkHttpClient
+            val req = Request.Builder().url("http://ytrw3xix.0g0.jp/app2017/feed").get().build()
+            val res = client.newCall(req).execute()
+            return res.body()?.string()
+        }catch(e: Exception){
+            return "error"
+        }
     }
 }
